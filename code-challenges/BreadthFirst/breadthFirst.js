82222ba7cb44002afdf0cef7ac5fe0819066e836
David Vloedman <djvloedman@gmail.com>
2020-01-28 14:41:35 -0800

    added tests for breadth first

--- code-challenges/BreadthFirst/breadthFirst.js
+++ code-challenges/BreadthFirst/breadthFirst.js
@@ -8,23 +8,27 @@ const Queue = require('../../data-structures/queue/queue');
  * @param {*} tree 
  */
 function breadthFirstList(tree){
-  breadthFirst(tree.tree.root);
+  return breadthFirst(tree.tree.root);
 }
 /**
  * 
  * @param {*} root 
  */
 function breadthFirst(root){
+  const values = new Array();
   const queue = new Queue();
-  if(!root) return;
+  if(!root) return null;
   queue.enqueue(root);
   while(!queue.isEmpty()){
     let node = queue.dequeue();
-    console.log(node.val);
+    values.push(node.val);
     if(node.left) queue.enqueue(node.left);
     if(node.right) queue.enqueue(node.right);
   }
+  return values;
 }
+
+module.exports = breadthFirstList;
 
 
 
