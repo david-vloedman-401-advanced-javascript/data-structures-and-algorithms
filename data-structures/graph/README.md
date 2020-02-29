0cd42526706b457e81bc84e1c9d917ae8c03e27d
David Vloedman <djvloedman@gmail.com>
2020-02-29 09:23:41 -0800

    Graph data structure

--- data-structures/graph/README.md
+++ data-structures/graph/README.md
@@ -2,24 +2,36 @@
 
 ## Create a directed graph
 
-[Pull Request](https://github.com/david-vloedman-401-advanced-javascript/data-structures-and-algorithms/pull/24)
+Create a graph data structure that can:  
+- add nodes
+- add edges
+- return all nodes
+- get all neighbors of a node
+- get the size of the graph
 
-### Directed Graph
+## Approach and Effiency 
 
-*
-*
-*
+My approach was pretty traditional, a lot of resources on this structure exist. 
 
-
-
-## Approach & Efficiency
-
-Each function scales at O(1) for both time and space.
-
-## Solution
+- addVertex - O(1)
+- addEdge - O(1)
+- getNodes - O(n)
+- getNeighbors - O(1)
+- size - O(1)
 
 
+## Directed Graph API
 
-![](./assets/graph.jpg)
+* addVertex(vertex) - adds a vertex or node to the graph
+* addEdge(vertex, node) - adds an edge between two vertices
+* getNodes() - returns all nodes within the graph
+* getNeighbors() - returns all the neighboring nodes of a given node
+* size() - returns the count of all nodes within the graph
+* print() - prints each node and it's neighbors in list form
+
+
+
+
+
 
 
--- data-structures/graph/graph.js
+++ data-structures/graph/graph.js
@@ -1,8 +1,16 @@
+/**
+ * @class Graph
+ * 
+ */
 class Graph {
   constructor() {
     this.adjList = new Map();
   }
-
+  /**
+   * Add a vertex to the grid
+   * @param {*} vertex 
+   * @returns number
+   */
   addVertex(vertex) {
     if (!this.adjList.has(vertex)) {
       this.adjList.set(vertex, []);
@@ -11,7 +19,12 @@ class Graph {
       return 1;
     }
   }
-
+  /**
+   * Add an edge between two vertices 
+   * @param {*} vertex 
+   * @param {*} node 
+   * @returns number
+   */
   addEdge(vertex, node) {
     if (this.adjList.has(vertex)) {
       if (this.adjList.has(node)) {
@@ -26,7 +39,10 @@ class Graph {
       return 1;
     }
   }
-
+  /**
+   * Gets all the nodes in the graph
+   * @returns nodes
+   */
   getNodes(){
     if(this.adjList.size === 0) return null;
     const nodes = [];
@@ -35,16 +51,25 @@ class Graph {
     }
     return nodes;
   }
-
+  /**
+   * Returns all neighbors of a given node
+   * @param {*} node 
+   * @return ndoes
+   */
   getNeighbors(node){
     if(!this.adjList.has(node)) return 1;
     return this.adjList.get(node);
   }
-
+  /**
+   * Returns the count of vertices within the graph
+   * @return size
+   */
   size(){
     return this.adjList.size;
   }
-
+  /**
+   * Print the nodes with their edges in list form
+   */
   print() {
     for (let [key, value] of this.adjList) {
       console.log(key, value);
